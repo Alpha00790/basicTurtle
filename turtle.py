0f3f84ee8e1a54231163bd39be133580238adc05
Shaikat Barua <49871819+Alpha00790@users.noreply.github.com>
2020-11-03 11:56:25 +0600

    🐢🐢🐢🐢

---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle
+a = 5
+print (a)
+
+
+harm = turtle.Turtle()
+harm.forward(100/2)
+harm.right(90)
+harm.forward(100/2)
+harm.right(90)
+harm.forward(100/2)
+harm.right(90)
+harm.forward(100/2)
